8260941c99885f1314f15753570469b6c71a85b3
jacksonmonteiro <infor.jackson324@gmail.com>
2026-03-08 14:30:32 -0300

    Holidays list ui

---
+++ app/src/main/java/com/jacksonmonteiro/holidaychecker/domain/model/Holiday.kt
@@ -0,0 +1,13 @@
+package com.jacksonmonteiro.holidaychecker.domain.model
+
+data class Holiday(
+    var date: String? = null,
+    var localName: String? = null,
+    var name: String? = null,
+    var countryCode: String? = null,
+    var fixed: Boolean = false,
+    var global: Boolean = false,
+    var counties: List<String> = emptyList(),
+    var launchYear: String? = null,
+    var types: List<String> = emptyList(),
+)
--- app/src/main/java/com/jacksonmonteiro/holidaychecker/presentation/holidays/HolidaysScreen.kt
+++ app/src/main/java/com/jacksonmonteiro/holidaychecker/presentation/holidays/HolidaysScreen.kt
@@ -1,6 +1,7 @@
 package com.jacksonmonteiro.holidaychecker.presentation.holidays
 
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,8 +9,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -29,10 +38,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jacksonmonteiro.holidaychecker.R
 import com.jacksonmonteiro.holidaychecker.domain.model.Country
+import com.jacksonmonteiro.holidaychecker.domain.model.Holiday
 import java.util.Calendar
 
 @Composable
@@ -85,6 +98,26 @@ fun HolidaysScreenContent(modifier: Modifier = Modifier) {
                     }
                 )
             }
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF18760C)
+                )
+            ) {
+                Text(
+                    text = "Search",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            HolidaysList()
         }
     }
 }
@@ -183,6 +216,69 @@ fun YearsSpinner(
                     text = { Text(year.toString()) },
                     onClick = { onSelectYear(year) }
                 )
+            }
+        }
+    }
+}
+
+@Composable
+fun HolidaysList(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Text(
+            text = "Holidays",
+            style = TextStyle(fontSize = 20.sp, color = Color.Black, fontWeight = FontWeight.Bold),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = modifier.height(16.dp))
+        LazyColumn(modifier.fillMaxWidth()) {
+            items(
+                listOf(
+                    Holiday(date = "01/01/2026", "Ano novo", "New Year"),
+                    Holiday(date = "25/12/2026", "Natal", "Christmas"),
+                )
+            ) { item ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(8.dp),
+                    border = BorderStroke(2.dp, Color.Gray),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFABE7A7))
+                ) {
+                    Column(modifier = Modifier.padding(8.dp)) {
+                        Row {
+                            Text("Data:", style = TextStyle(color = Color.Black, fontSize = 16.sp, fontWeight = FontWeight.Medium))
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                item.date.orEmpty(),
+                                style = TextStyle(color = Color.Black, fontSize = 16.sp)
+                            )
+                        }
+                        Row {
+                            Text(
+                                "Nome Local do Feriado:",
+                                style = TextStyle(color = Color.Black, fontSize = 16.sp, fontWeight = FontWeight.Medium)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                item.localName.orEmpty(),
+                                style = TextStyle(color = Color.Black, fontSize = 16.sp)
+                            )
+                        }
+                        Row {
+                            Text(
+                                "Nome Global do Feriado:",
+                                style = TextStyle(color = Color.Black, fontSize = 16.sp, fontWeight = FontWeight.Medium)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                item.name.orEmpty(),
+                                style = TextStyle(color = Color.Black, fontSize = 16.sp)
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
             }
         }
     }
